35bc3383e9823c5dbef66a6c2cb03aad6aebec2c
Krzysztof Kirsz <k.kirsz@vector.net>
2022-02-18 22:43:18 +0100

    Fix test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f output_file
-cd build && cmake .. && make -j3 && ./tests && ./copy_file ../random_file ../output_file
+mkdir -p build && cd build && cmake .. && make -j3 && ./tests && ./copy_file ../random_file ../output_file
 cd ..
 if cmp -s "random_file" "output_file"; then
   printf "Files are the same."
